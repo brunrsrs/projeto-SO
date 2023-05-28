b12e8c019b8b6ca0d9a5e3673afa9a2cc4f8c8ee
Bruno Silveira <93217089+brunrsrs@users.noreply.github.com>
2023-05-28 13:51:30 -0300

    agora lê corretamente os arquivos e separa eles

--- functions.h
+++ functions.h
@@ -2,23 +2,30 @@ typedef struct programa {
     char nome[20];
     int ident;
     int prioridade;
-    int tamanho;//*
     int tempo;
+    int tamanho;
     char semaforos[5];//coloquei esses 2 parametros aqui, de acordo com o gpt isso é o bcp já
-} pg;
+} programa;
 
+
+typedef struct bcp {
+    programa *prog;
+    int tamTotal;
+    struct bcp *prox;
+} bcp;
+
+
+//funções de controle
 void menu();
+int inicializaBCP(bcp*);
 
+//funções do banco de dados
+int programRead(struct programa*, char*, struct bcp*);
+void processCreate(struct programa*, struct bcp*);
+void memLoadReq(struct programa*, struct bcp*);
 int interruptControl(); //retorna valor que ativa a thread
-
 void processInterrupt();
-
-void memLoadReq(pg);
-
 void memLoadFinish();
+void processFinish(struct programa, struct bcp);
 
-//void processCreate(pg);
-
-void processFinish(pg);
-
-void programRead(char);
+//funções do escalonador
